962a7fdd59fcd518dea2eb74033127c03c047908
David Hoppenbrouwers <david@salt-inc.org>
2024-06-11 11:12:23 +0200

    fix array example

--- script/src/lib.rs
+++ script/src/lib.rs
@@ -481,7 +481,7 @@ mod test {
         let s = include_str!("../examples/hello_world.pil");
         let s = include_str!("../examples/union.pil");
         let s = include_str!("../examples/group.pil");
-        //let s = include_str!("../examples/array.pil");
+        let s = include_str!("../examples/array.pil");
         col.parse_text("", s).unwrap();
         dbg!(&col);
         let mut prog = super::Program::from_collection(&col, &"start".to_string().into()).unwrap();
